37477e4feba5af88c35e67796307ba64ba96fc3d
Hakan ÖZCAN <hozcan@tumsas.local>
2023-03-14 11:54:13 +0300

    a

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdersService } from '../orders.service';
+constructor(private orderService: OrderService) {}
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.css']
 })
+
 export class OrderListComponent implements OnInit {
   orders: Order[] = [];
 
