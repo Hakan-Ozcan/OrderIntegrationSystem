a716c6520269dfe66e19e20a85e6d77ab8347dbb
Hakan ÖZCAN <hozcan@tumsas.local>
2023-03-14 11:37:57 +0300

    bazı değişiklikler

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../orders.service';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.css']
 })
-export class OrderListComponent {
+export class OrderListComponent implements OnInit {
+  orders: Order[] = [];
 
+  constructor(private orderService: OrdersService) {}
+
+  ngOnInit(): void {
+    this.orderService.getOrders().subscribe(orders => {
+      this.orders = orders;
+    });
+  }
 }
