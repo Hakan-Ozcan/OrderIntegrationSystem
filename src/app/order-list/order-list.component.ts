a8bcc57c1611f05ad3d21548c796bed2b278e75e
Hakan ÖZCAN <hozcan@tumsas.local>
2023-03-14 12:07:47 +0300

    d

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,21 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { OrdersService } from '../orders.service';
-constructor(private orderService: OrderService) {}
+import { Order } from '../order';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.css']
 })
-
 export class OrderListComponent implements OnInit {
   orders: Order[] = [];
 
-  constructor(private orderService: OrdersService) {}
+  constructor(private orderService: OrderService) {}
 
-  ngOnInit(): void {
-    this.orderService.getOrders().subscribe(orders => {
-      this.orders = orders;
-    });
+  ngOnInit() {
+    this.getOrders();
+  }
+
+  getOrders(): void {
+    this.orderService.getOrders().subscribe((orders) => (this.orders = orders));
+  }
+
+  onEdit(order: Order): void {
+    order.editable = true;
+  }
+
+  onCancelEdit(order: Order): void {
+    order.editable = false;
+  }
+
+  onStatusChange(order: Order, newStatus: string): void {
+    order.status = newStatus;
+    order.editable = false;
+    this.orderService.updateOrderStatus(order.id, order.status).subscribe();
   }
 }
