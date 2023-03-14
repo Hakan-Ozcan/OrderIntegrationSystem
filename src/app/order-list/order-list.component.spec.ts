2a7808eee315328d780382e8d51723f9bcf50cad
Hakan ÖZCAN <hozcan@tumsas.local>
2023-03-14 11:19:00 +0300

    service cıkarıldı

--- src/app/order-list/order-list.component.spec.ts
+++ src/app/order-list/order-list.component.spec.ts
@@ -2,6 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderListComponent } from './order-list.component';
 
+editStatus(order: Order) {
+  const dialogRef = this.dialog.open(EditStatusDialogComponent, {
+    data: {order}
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+    if (result) {
+      this.orderService.updateOrderStatus(order.id, result).subscribe(updatedOrder => {
+        const index = this.orders.findIndex(o => o.id === updatedOrder.id);
+        this.orders[index] = updatedOrder;
+      });
+    }
+  });
+}
 describe('OrderListComponent', () => {
   let component: OrderListComponent;
   let fixture: ComponentFixture<OrderListComponent>;
@@ -21,3 +35,4 @@ describe('OrderListComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
